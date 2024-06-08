aa9c09eeecbb5dffb0b350721412dd439abb78c1
OfentseXI <dofentse69@gmail.com>
2024-06-08 15:22:23 +0200

    vanElements function created and defined

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -9,7 +9,16 @@ export default function Vans() {
             .then(data => console.log(data))
     }, [])
 
-    
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
 
     return (
         <h1>Vans page goes here 🚐</h1>
