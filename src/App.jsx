0d464043e8c1b5c7abf037cf3160f761a4e9e1a6
OfentseXI <dofentse69@gmail.com>
2024-06-08 14:36:52 +0200

    Home and about links added to browswerRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,23 @@ import About from "../pages/About"
 function App() {
 
   return (
-    <h1>Start here</h1>
-  )
+    <BrowserRouter>
+      <header>
+          <Link to = "/">#VANLIFE</Link>
+        <nav>
+          <Link to = "/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/about" element = {<About />}/>
+      </Routes>
+    </BrowserRouter>
+   )
 }
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />)
 
 export default App
