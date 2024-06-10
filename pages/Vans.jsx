3285758d87250a057a6fed5d669e44e74be70b21
OfentseXI <dofentse69@gmail.com>
2024-06-10 08:18:25 +0200

    App.jsx export and mirage.js install

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -3,7 +3,6 @@ import React from "react"
 export default function Vans() {
     const [vans, setVans] = React.useState([])
     React.useEffect(() => {
-        
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => console.log(data))
